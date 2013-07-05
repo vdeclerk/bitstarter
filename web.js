e84457a7f948d8c0a74f381b153819b6de27a664
Victor Declerk <victor.declerk@gmail.com>
2013-07-05 10:37:17 -0300

    Modified web.js to read index.html

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var fs = require('fs');
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var file = fs.readFileSync("index.html");
+    response.send(file.toString());
 });
 
 var port = process.env.PORT || 5000;
