3c07b8f5c6cd2f745677f86067bb445a51dab8ae
Victor Declerk <victor.declerk@gmail.com>
2013-07-07 14:30:03 -0300

    Grader updated.

--- grader.js
+++ grader.js
@@ -24,8 +24,11 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var HTMLURL_DEFAULT = "http://www.google.com";
+var DOWNLOADED_FILE = "downloaded-bitstarter-grader.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -60,15 +63,36 @@ var clone = function(fn) {
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
+var downloaded = function(result) {
+	if (result instanceof Error) {
+	    console.error('Error: ' + util.format(response.message));
+	} else {
+	    console.error("Wrote %s", DOWNLOADED_FILE);
+	    fs.writeFileSync(DOWNLOADED_FILE, result);
+	    doCheck(DOWNLOADED_FILE, program.checks);
+	}
+    };
+
+var doCheck = function(file, checks) {
+    var checkJson = checkHtmlFile(file, checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+};
 
 if(require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <html_url>', 'URL of file to check', HTMLURL_DEFAULT)
 	.parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    console.log(program.checks);
+    console.log(program.file);
+    console.log(program.url);
+    if (program.url) {
+	rest.get(program.url).on('complete', downloaded);
+    } else {
+	doCheck(program.file, program.checks);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
